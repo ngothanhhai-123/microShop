0f16773e8fcbe9dc212a4da9e0b9be493843b60d
ngothanhhai-123 <ngothanhhai12032005@gmail.com>
2026-03-08 15:43:07 +0700

    test1

--- src/main/java/com/microshop/filter/AuthenticationFilter.java
+++ src/main/java/com/microshop/filter/AuthenticationFilter.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 // ok
+//ok
 @WebFilter(urlPatterns = {"/profile/*", "/payment/*"})
 public class AuthenticationFilter implements Filter {
     @Override
